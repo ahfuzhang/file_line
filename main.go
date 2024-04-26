0cdcac81f98d82b69a46cd2f3941ecae45141857
fuchun.zhang <fuchun.zhang@bytedance.com>
2024-04-26 13:52:14 +0800

    bug fix: use abs path

--- main.go
+++ main.go
@@ -10,35 +10,51 @@ import (
 	"go/token"
 	"log"
 	"os"
-	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
 var (
 	src           = flag.String("src", "./", "source dir or file")
+	exclude       = flag.String("exclude", "", "exclude path names, separated by commas")
 	filePatternRE = regexp.MustCompile(`\[[A-Za-z0-9_\-\.\\\/]+\.go\:\d+\]`)
 )
 
-func readDir(d string) {
+func readDir(d string, prefixLen int) {
+	if len(d) == 0 {
+		log.Printf("empty dir name")
+		return
+	}
+	if !strings.HasPrefix(d, "..") && d[0] == '.' {
+		log.Printf("skip hidden dir: %s", d)
+		return
+	}
 	dirs, err := os.ReadDir(d)
 	if err != nil {
-		log.Printf("read dir %s error, err=%s", d, err)
+		cur, _ := os.Getwd()
+		log.Printf("read dir %s error, err=%s\n\t%s", d, err, cur)
 	}
-	b := path.Base(d)
+	//b := path.Base(d)
 	for _, item := range dirs {
 		if item.IsDir() {
-			readDir(path.Join(b, item.Name()))
+			if _, has := excludePaths[item.Name()]; has {
+				continue
+			}
+			newDir := filepath.Join(d, item.Name())
+			//fmt.Printf("  read: %s\n", newDir)
+			readDir(newDir, prefixLen)
 			continue
 		}
 		if strings.HasSuffix(item.Name(), ".go") {
-			f := path.Join(b, item.Name())
-			readFile(f)
+			f := filepath.Join(d, item.Name())
+			//fmt.Printf("  read: %s\n", f)
+			readFile(f, prefixLen)
 		}
 	}
 }
 
-func readFile(f string) {
+func readFile(f string, prefixLen int) {
 	fset := token.NewFileSet()
 	file, err := parser.ParseFile(fset, f, nil, parser.ParseComments)
 	if err != nil {
@@ -55,12 +71,12 @@ func readFile(f string) {
 						lit.Value[1] == '[' && lit.Value[len(lit.Value)-2] == ']' &&
 						filePatternRE.MatchString(lit.Value) {
 						pos := fset.Position(n.Pos())
-						replaceTo := fmt.Sprintf("\"[%s:%d]\"", pos.Filename, pos.Line)
+						replaceTo := fmt.Sprintf("\"[%s:%d]\"", pos.Filename[prefixLen:], pos.Line)
 						if lit.Value == replaceTo {
 							continue
 						}
 						isModify = true
-						log.Printf("Found a constant string argument:%+v, replace to \"[%s:%d]\"\n", lit.Value, pos.Filename, pos.Line)
+						log.Printf("Found a constant string argument:%+v, replace to \"[%s:%d]\"\n", lit.Value, pos.Filename[prefixLen:], pos.Line)
 						newArg := &ast.BasicLit{
 							Kind:  token.STRING,
 							Value: replaceTo,
@@ -85,6 +101,8 @@ func readFile(f string) {
 	}
 }
 
+var excludePaths = map[string]struct{}{}
+
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	flag.Parse()
@@ -96,11 +114,22 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+	if len(*exclude) > 0 {
+		arr := strings.Split(*exclude, ",")
+		for _, item := range arr {
+			excludePaths[strings.Trim(item, " ")] = struct{}{}
+		}
+	}
 	if info.IsDir() {
-		readDir(*src)
+		a, err := filepath.Abs(*src)
+		if err != nil {
+			log.Println("get abs path error:", err, *src)
+			return
+		}
+		readDir(a, len(a))
 		return
 	}
 	if strings.HasSuffix(*src, ".go") {
-		readFile(*src)
+		readFile(*src, 0)
 	}
 }
